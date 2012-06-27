8c0a15ea94aa9c2867729ab5f15bee2841735f9e
Hendy Irawan <hendy@soluvas.com>
2012-06-27 20:19:36 +0700

    home address

--- cli/src/main/java/org/soluvas/ldaptools/cli/EntryAdder.java
+++ cli/src/main/java/org/soluvas/ldaptools/cli/EntryAdder.java
@@ -1,5 +1,6 @@
 package org.soluvas.ldaptools.cli;
 
+import java.util.UUID;
 import java.util.concurrent.Callable;
 
 import javax.inject.Inject;
@@ -45,12 +46,39 @@ public class EntryAdder {
 
 				DefaultEntry addresses = new DefaultEntry(new Dn(new Rdn("ou=addresses"), entry.getDn()));
 				addresses.add("objectClass", "organizationalUnit");
-				log.debug("Adding addresses LDAP Entry {}", addresses.getDn().getName());
+				log.debug("Adding addresses LDAP Entry {}", addresses.getDn());
 				ldap.add(addresses);
+				
+				String homeAddressUid = UUID.randomUUID().toString();
+				DefaultEntry homeAddress = new DefaultEntry(new Dn(new Rdn("uniqueIdentifier=" + homeAddressUid), addresses.getDn()));
+				homeAddress.add("objectClass", "address", "extensibleObject");
+				homeAddress.add("description", "Rumah");
+				if (entry.containsAttribute("cn"))
+					homeAddress.add(entry.get("cn"));
+				if (entry.containsAttribute("o"))
+					homeAddress.add(entry.get("o"));
+				if (entry.containsAttribute("street"))
+					homeAddress.add(entry.get("street"));
+				if (entry.containsAttribute("l"))
+					homeAddress.add(entry.get("l"));
+				if (entry.containsAttribute("st"))
+					homeAddress.add(entry.get("st"));
+				if (entry.containsAttribute("c"))
+					homeAddress.add(entry.get("c"));
+				if (entry.containsAttribute("mobile"))
+					homeAddress.add(entry.get("mobile"));
+				if (entry.containsAttribute("telephoneNumber"))
+					homeAddress.add(entry.get("telephoneNumber"));
+				if (entry.containsAttribute("facsimileTelephoneNumber"))
+					homeAddress.add(entry.get("facsimileTelephoneNumber"));
+				if (entry.containsAttribute("mail"))
+					homeAddress.add(entry.get("mail"));
+				log.debug("Adding address LDAP Entry {}", homeAddress.getDn());
+				ldap.add(homeAddress);
 				
 				DefaultEntry payments = new DefaultEntry(new Dn(new Rdn("ou=payments"), entry.getDn()));
 				payments.add("objectClass", "organizationalUnit");
-				log.debug("Adding addresses LDAP Entry {}", addresses.getDn().getName());
+				log.debug("Adding addresses LDAP Entry {}", addresses.getDn());
 				ldap.add(payments);
 			
 				return entry;
