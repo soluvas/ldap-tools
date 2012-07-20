c575065266000f5ef4f193b4ee63b70dbc35cb3d
Hendy Irawan <hendy@soluvas.com>
2012-07-20 16:23:17 +0700

    * Assembly * Upgrade dependencies.

--- cli/ldapcli.sh
+++ cli/ldapcli.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Must run first: mvn package dependency:copy-dependencies
-java -cp 'target/dependency/*:target/ldap-cli-1.0.0-SNAPSHOT.jar' org.jboss.weld.environment.se.StartMain "$@"
+java -cp 'lib/*' org.jboss.weld.environment.se.StartMain "$@"
